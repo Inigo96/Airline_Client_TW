59bbe95c99550f15a6656da7acdd0fc4a53e363f
inigo <inigo.alon@opendeusto.es>
2016-12-30 13:07:02 +0100

    subir

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -12,82 +12,11 @@ public class Controller {
 
 	private RMIServiceLocator rsl;
 
-	public SMSController(String[] args) throws RemoteException {
+	public Controller(String[] args) throws RemoteException {
 		rsl = new RMIServiceLocator();
 		rsl.setService(args);
 		new SMSWindow(this);
 	}
 
-	public List<TVProgramDTO> getTVPrograms() {
-		List<TVProgramDTO> programs = new ArrayList<>();
-		// ADD YOUR CODE HERE
-		try {
-			for (TVProgramDTO a : rsl.getAdminService().getTVPrograms()) programs.add(a);
-			//for(TVProgramDTO a : rsl.getReceiverService().getTVPrograms()) programs.add(a);
-		}catch (Exception e){
 
-		}
-		return programs;
-	}
-
-	public void newTVProgram(String acronym, String description) {
-		// ADD YOUR CODE HERE
-		try {
-			// Add your code HERE - Related to getting the service and requesting creation of TVProgram
-			//rsl.getService().newTVProgram( acronym, description);
-			rsl.getAdminService().newTVProgram( acronym, description);
-		} catch (Exception e){
-			System.err.println("$ Error sending new TV program: " + e.getMessage());
-		}
-	}
-
-	public void sendMessage(String phone, String text) {
-		// ADD YOUR CODE HERE
-		try{
-			// Add your code HERE - Related to getting the service and sending a message
-			rsl.getReceiverService().receiveMessage(phone, text);
-
-
-		} catch(Exception e){
-			System.out.println("$ Error sending a message: " + e.getMessage());
-		}
-	}
-
-	public void closeTVProgram(String acro) {
-
-		// ADD YOUR CODE HERE
-
-	}
-
-	public void addMaximumNumberMessagesRestriction() {
-
-		// ADD YOUR CODE HERE
-
-	}
-
-	public void addPermittedOptionsRestriction() {
-
-		// ADD YOUR CODE HERE
-
-	}
-
-	public void addForbiddenWordsRestriction() {
-
-		// ADD YOUR CODE HERE
-	
-	}
-
-	public void generateReport() {
-
-		// ADD YOUR CODE HERE
-
-	}
-
-	public void exit() {
-		System.exit(0);
-	}
-
-	public static void main(String[] args) throws RemoteException {
-		new SMSController(args);
-	}
 }
--- src/remote/RMIServiceLocator.java
+++ src/remote/RMIServiceLocator.java
@@ -5,22 +5,22 @@ import es.deusto.ingenieria.sd.sms.server.remote.ITVProgramReceiver;
 
 public class RMIServiceLocator{
 	// The Cache
-	private ReservationService reservationService;
+	private IReservationService reservationService;
 
     public void setService(String[] args) {
     	
     	// Add your code to get the TARGET reference HERE
         try {
             String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
-            adminService = (ReservationService) java.rmi.Naming.lookup(name);
+            this.reservationService = (IReservationService) java.rmi.Naming.lookup(name);
 
         }catch (Exception e){
             //System.out.println("//" + args[0] + ":" + args[1] + "/" + args[2]);
         }
     }
 
-    public ReservationService getReservationService() {
-    	return ReservationService;
+    public IReservationService getIReservationService() {
+    	return IReservationService;
     }
 
 }
