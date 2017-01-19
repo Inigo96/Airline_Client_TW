316c1c2c44baba4dba7fcb2fc9936092228d612e
inigo <inigo.alon@opendeusto.es>
2017-01-19 01:32:39 +0100

    quitar duplicado

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -2,9 +2,7 @@ package gui;
 
 import controller.Controller;
 import entities.FlightDTO;
-import entities.Reserva;
 import entities.UserDTO;
-import entities.Vuelo;
 import entities.ReservationDTO;
 
 import javax.swing.JFrame;
@@ -92,12 +90,13 @@ public class MainWindow extends JFrame implements ActionListener{
 	}
 	
 	private void rellenarTablaReservas(ArrayList<ReservationDTO> reservas){
-		
-		// TODO Hay un problema de casting bb
-		ReservationDTO[] resers=(ReservationDTO[]) reservas.toArray();
+
+//		ReservationDTO[] resers=new ReservationDTO[reservas.size()];
+
+//		ReservationDTO[] resers=(ReservationDTO[]) reservas.toArray();
 		for(int i=0;i<reservas.size();i++){
 			FlightDTO f;
-			f=resers[i].getFlight();
+			f=reservas.get(i).getFlight();
 			table.setValueAt(Integer.toString(i), i, 0);
 			table.setValueAt(f.toString(), i, 1);
 
@@ -284,223 +283,4 @@ public class MainWindow extends JFrame implements ActionListener{
 
 		return Integer.toString(day)+"-"+Integer.toString(month)+"-"+Integer.toString(year);
 	}
-}
-
-	private JComboBox diaComBox;
-	private JComboBox mesComBox;
-	private JComboBox anyoComBox;
-
-
-	public MainWindow(Controller controller){
-		this.controller = controller;
-		initComponents();
-		this.setVisible(true);
-		this.centreWindow();
-	}
-
-	public void initComponents(){
-		setResizable(true);
-//		getContentPane().setLayout(null);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.setSize(700,700);
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 0, 434, 125);
-		getContentPane().add(panel_1);
-		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
-
-		JPanel panel_2 = new JPanel();
-		panel_1.add(panel_2);
-		panel_2.setLayout(null);
-
-		btnLogIn = new JButton("Log in");
-		btnLogIn.setBounds(118, 91, 89, 23);
-		panel_2.add(btnLogIn);
-
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(82, 22, 125, 20);
-		panel_2.add(txtUsuario);
-		txtUsuario.setColumns(10);
-
-		txtPass = new JTextField();
-		txtPass.setBounds(82, 53, 125, 20);
-		panel_2.add(txtPass);
-		txtPass.setColumns(10);
-
-		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(26, 25, 46, 14);
-		panel_2.add(lblUsuario);
-
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setBounds(10, 56, 62, 14);
-		panel_2.add(lblContrasea);
-
-		btnReservas = new JButton("Reservas");
-		btnReservas.setBounds(20, 91, 99, 23);
-		panel_2.add(btnReservas);
-
-		JPanel panel_3 = new JPanel();
-		panel_1.add(panel_3);
-		panel_3.setLayout(null);
-
-		btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(131, 91, 86, 23);
-
-		panel_3.add(btnBuscar);
-
-		txtDestino = new JTextField();
-		txtDestino.setText("Destino");
-		txtDestino.setBounds(131, 24, 86, 20);
-		panel_3.add(txtDestino);
-		txtDestino.setColumns(10);
-
-		txtOrigen = new JTextField();
-		txtOrigen.setText("Origen");
-		txtOrigen.setBounds(10, 24, 86, 20);
-		panel_3.add(txtOrigen);
-		txtOrigen.setColumns(10);
-
-		JLabel barra = new JLabel("/");
-		barra.setBounds(62, 58, 23, 14);
-		panel_3.add(barra);
-
-		diaComBox = new JComboBox();
-		diaComBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
-		diaComBox.setBounds(10, 55, 42, 20);
-		panel_3.add(diaComBox);
-
-		mesComBox = new JComboBox();
-		mesComBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
-		mesComBox.setBounds(82, 55, 42, 20);
-		panel_3.add(mesComBox);
-
-		anyoComBox = new JComboBox();
-		anyoComBox.setModel(new DefaultComboBoxModel(new String[] {"2017", "2018"}));
-		anyoComBox.setBounds(156, 55, 61, 20);
-		panel_3.add(anyoComBox);
-
-		JLabel barra_1 = new JLabel("/");
-		barra_1.setBounds(134, 58, 12, 14);
-		panel_3.add(barra_1);
-
-		btnReservar = new JButton("Reservar");
-		btnReservar.setBounds(10, 91, 100, 23);
-		panel_3.add(btnReservar);
-
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 125, 444, 136);
-		getContentPane().add(panel);
-		panel.setLayout(new BorderLayout(0, 0));
-
-		JLabel lblResultados = new JLabel("  Resultados");
-		panel.add(lblResultados, BorderLayout.NORTH);
-
-		scrollPane = new JScrollPane();
-		panel.add(scrollPane, BorderLayout.CENTER);
-
-//		table = new JTable();
-		scrollPane.setVisible(true);
-//		table.setVisible(true);
-
-		scrollPane.setViewportView(table);
-
-		btnBuscar.addActionListener(this);
-		btnLogIn.addActionListener(this);
-		btnReservas.addActionListener(this);
-	}
-
-	private JTable rellenarTablaVuelos(ArrayList<FlightDTO> a){
-		JTable tabla;
-		String[] columnas = {"Departure", "Arrival", "Date"};
-		String[][] datos = new String [a.size()][numColumnasVuelos];
-		for(int i = 0; i< a.size(); i++){
-				FlightDTO v =  a.get(i);
-				datos[i][0] = v.getArrivalA();
-				datos[i][1] = v.getDepartureA();
-				datos[i][2] = gregorianToString(v.getDate());
-		}
-		tabla = new JTable(datos, columnas);
-		return tabla;
-	}
-
-	private JTable rellenarTablaReservas(ArrayList<ReservationDTO> a){
-		JTable tabla;
-		String[] columnas = {"Reservation number", "Flights"};
-		String[][]datos= new  String[a.size()][numColumnasVuelos];
-		ReservationDTO[] reservas=(ReservationDTO[]) a.toArray();
-		for(int i=0;i<a.size();i++){
-			FlightDTO f;
-			f=reservas[i].getFlight();
-			datos[i][0] = Integer.toString(i);
-			datos[i][1]=f.toString();
-
-		}
-		tabla = new JTable(datos, columnas);
-		return tabla;
-
-	}
-
-
-	public void actionPerformed(ActionEvent e) {
-		user = controller.login(txtUsuario.getText(), txtPass.getText());
-		if(e.getSource().equals(btnBuscar)){
-			// conexion con los datos
-			flightList= controller.searchFlight(txtOrigen.getText(), txtDestino.getText(), new GregorianCalendar(anyoComBox.getSelectedIndex(), mesComBox.getSelectedIndex(),
-					 diaComBox.getSelectedIndex()));
-
-			System.out.println("flightList = " + flightList);
-			System.out.println("flightList = " + flightList.size());
-			table =rellenarTablaVuelos(flightList);
-			this.scrollPane.add(table);
-			table.setVisible(true);
-
-
-		}else if(e.getSource().equals(btnLogIn)){
-			String email = txtUsuario.getText();
-			String pass = txtPass.getText();
-			// Comprueba que el usuario va con la contrasena.
-			if(user!=null){
-				txtUsuario.setEditable(false);
-				txtPass.setEditable(false);
-				this.setTitle("Log in correct");
-//				try{wait(3000);}catch(InterruptedException exception){
-//					exception.printStackTrace();
-//				}
-//				this.setTitle("");
-
-			}else{
-				this.setTitle("Error in Log in");
-//				try{wait(3000);}catch(InterruptedException exception){
-//					exception.printStackTrace();
-//				}
-//				this.setTitle("");
-			}
-
-		}else if(e.getSource().equals(btnReservas)){
-			// Conseguir las reservas en funcion del User
-			reservationList = controller.getReservations(user);
-			rellenarTablaReservas(reservationList);
-
-		}else if(e.getSource().equals(btnReservar)){
-			// Crear reserva
-			int numVuelo = table.getSelectedRow();
-			FlightDTO flight  = flightList.get(numVuelo);
-			controller.createReservation(flight, user);
-
-		}
-	}
-
-	public void centreWindow() {
-		Dimension dim = getToolkit().getScreenSize();
-		Rectangle abounds = getBounds();
-		setLocation((dim.width - abounds.width) / 2, (dim.height - abounds.height) / 2);
-	}
-	public String gregorianToString(GregorianCalendar calendar){
-
-		int year =calendar.get(Calendar.YEAR);
-		int month= calendar.get(Calendar.MONTH)+1;
-		int day=calendar.get(Calendar.DAY_OF_MONTH);
-
-		return Integer.toString(day)+"-"+Integer.toString(month)+"-"+Integer.toString(year);
-	}
-
 }
